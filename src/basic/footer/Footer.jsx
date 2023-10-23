6f22663bd8fbeecdf93595a8ed5e4c865f1577e6
=ahmedyasser <=ahmedya3ser8@gmail.com>
2023-10-23 07:55:21 +0300

    create footer and end home page

--- src/basic/footer/Footer.jsx
+++ src/basic/footer/Footer.jsx
@@ -1,7 +1,29 @@
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <footer className="bg-[#f0f3f2] py-8">
+      <div className="container">
+        <div className="footer-head border-b-[1px] border-solid border-[#ccc] pb-10">
+          <h3 className="text-[25px] mb-[10px]">Welcome ahmed Get the FreshCart app</h3>
+          <p className="text-[18px] text-[#777] mb-[10px]">we will send you a link , open it on your phone to download the app</p>
+          <div className="flex flex-col lg:flex-row gap-6">
+            <input type="text" id="footer-input" className="w-full h-[40px] rounded-[10px] outline-none p-2" />
+            <button className="w-full md:w-[180px] text-[18px] h-[40px] p-2 bg-[#0aad04] text-white rounded-[10px]">Share App</button>
+          </div>
+        </div>
+        <div className="footer-bottom flex justify-between items-center flex-col lg:flex-row pt-10">
+          <div className="left flex items-center gap-5">
+            <span className="text-[#777]">Payment Partners</span>
+            <img src="/public/images/pay.webp" alt="pay-img" />
+          </div>
+          <div className="right flex items-center gap-5">
+            <span className="text-[#777] text-[14px]">Get deliveries with FreshCart</span>
+            <img src="/public/images/app-store/appStore.png" alt="appStore"  className="w-[100px]"/>
+            <img src="/public/images/app-store/googlePlay.png" alt="googlePlay" className="w-[100px]" />
+          </div>
+        </div>
+      </div>
+    </footer>
   )
 }
 
--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -5,7 +5,7 @@ function Products() {
   return (
     <section className="py-12">
       <div className="container">
-        <div className="products-wrapper grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-5">
+        <div className="products-wrapper grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-4">
           {products.map(product => (
             <div className="product-content p-[20px] border-2 border-solid border-[#ccc] relative" key={product.id}>
               <AiOutlineHeart className='absolute right-1 top-2 text-[#0aad0a] text-[22px] cursor-pointer'/>
@@ -14,7 +14,7 @@ function Products() {
               </div>
               <div className="product-text mb-[5px]">
                 <h4 className='text-[#0aad0a]'>{product.title}</h4>
-                <h5 className='text-[15px] text-[#777]'>{product.text}</h5>
+                <h5 className='text-[13px] text-[#777]'>{product.text}</h5>
               </div>
               <div className="product-info flex justify-between items-center mb-[10px]">
                 <span className='text-[#777]'>{`${product.price} EGP`}</span>
